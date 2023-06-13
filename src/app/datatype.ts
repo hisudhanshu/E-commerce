11a312b2135e17a08dbe47b4c34e775d28af59fb
Sudhanshu Bharti <105193952+hisudhanshu@users.noreply.github.com>
2023-06-13 17:02:37 +0530

    Add datatype.ts file 7th Day

--- src/app/datatype.ts
+++ src/app/datatype.ts
@@ -1,6 +1,9 @@
 // Replace the signUp interface from seller-auth comp and service for data:any == data:signUp
-export interface signUp{
+
+export interface signUp {
+    
     name:string,
     password:string,
     email:string
+
 }
